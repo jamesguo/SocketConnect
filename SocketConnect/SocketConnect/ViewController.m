cc5e837bb8613cf1bde90aafee4b368bd71d2e3b
Linda Li <linda.i.ling@ericsson.com>
2014-04-30 21:55:14 +0800

    add swap byte to int method

--- SocketConnect/SocketConnect/ViewController.m
+++ SocketConnect/SocketConnect/ViewController.m
@@ -27,6 +27,32 @@
     self.mainTableView.delegate = self;
     
     self.title=@"Demo";
+    
+    long int blah = 3840;
+    char* pBlah = (char*)&blah;
+    
+    printf("Default\n");
+    printf("%d\n", pBlah [0]);
+    printf("%d\n", pBlah [1]);
+    printf("%d\n", pBlah [2]);
+    printf("%d\n", pBlah [3]);
+    
+    unsigned char bytes[4];
+    bytes[0] = (blah >> 24) & 0xFF;
+    bytes[1] = (blah >> 16) & 0xFF;
+    bytes[2] = (blah >> 8) & 0xFF;
+    bytes[3] = blah & 0xFF;
+    
+    printf("Second\n");
+    printf("%d\n", bytes [0]);
+    printf("%d\n", bytes [1]);
+    printf("%d\n", bytes [2]);
+    printf("%d\n", bytes [3]);
+    
+    int result = bytes[0]<<24|bytes[1]<<16|bytes[2]<<8|bytes[3];
+    printf("result:%d\n", result);
+    result = pBlah[3]<<24|pBlah[2]<<16|pBlah[1]<<8|pBlah[0];
+    printf("result:%d\n", result);
 }
 
 - (void)didReceiveMemoryWarning
