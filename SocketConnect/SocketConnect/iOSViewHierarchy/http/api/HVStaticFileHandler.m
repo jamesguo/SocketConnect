33ba7b63305fe8e30bde6bc2ffe3435990bac58e
jamesguo <james.guo89@gmail.com>
2014-05-06 09:56:33 +0800

    Add Http Server

---
+++ SocketConnect/SocketConnect/iOSViewHierarchy/http/api/HVStaticFileHandler.m
@@ -0,0 +1,47 @@
+//
+//  HVStaticFileHandler.m
+//
+//  Copyright (c) 2012 Damian Kolakowski. All rights reserved.
+//
+
+#import "HVStaticFileHandler.h"
+#import "HVBaseRequestHandler.h"
+
+@implementation HVStaticFileHandler : HVBaseRequestHandler
+
++ (HVStaticFileHandler *)handler:(NSString *)filePath
+{
+  return [[HVStaticFileHandler alloc] initWithFileName:filePath] ;
+}
+
+- (id)initWithFileName:(NSString *)filePath
+{
+  self = [super init];
+  if (self) {
+//    file = [filePath retain];
+  }
+  return self;
+}
+
+- (void)dealloc
+{
+
+}
+
+- (BOOL)handleRequest:(NSString *)url withHeaders:(NSDictionary *)headers query:(NSDictionary *)query address:(NSString *)address onSocket:(int)socket
+{
+  if ([super handleRequest:url withHeaders:headers query:query address:address onSocket:socket]) {
+    NSString *filePath = [[NSBundle mainBundle] pathForResource:file ofType:nil];
+    if (filePath) {
+      NSData *data = [[NSData alloc] initWithContentsOfFile:filePath];
+      if (data) {
+        [self writeData:data toSocket:socket];
+
+      }
+    }
+    return YES;
+  }
+  return NO;
+}
+
+@end
